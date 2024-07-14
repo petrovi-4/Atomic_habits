fc7a80a4aead1b03bcc2909150ce7dfbc288b034
petrovi4 <40.10.444@gmail.com>
2024-07-14 08:29:28 +0300

    add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from config.settings import NULLABLE
+
+
+class Useer(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    telegram_chat_id = models.CharField(max_length=35, verbose_name='ID чата Telegram', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
