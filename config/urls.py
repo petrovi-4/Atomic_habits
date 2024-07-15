e1e551c22cf3eadffb00e966eb1def52f6e3630d
petrovi4 <40.10.444@gmail.com>
2024-07-15 14:13:03 +0300

    add README.md

--- config/urls.py
+++ config/urls.py
@@ -38,5 +38,6 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='user')),
     path('habit/', include('habits.urls', namespace='habit')),
 
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
